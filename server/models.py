21da6e808a5d0070b418bcae3f8de4dc0a816afa
WanjikuN <cikunjoroge4@gmail.com>
2023-10-19 16:11:43 +0300

    feat: add serialization

--- server/models.py
+++ server/models.py
@@ -11,10 +11,20 @@ db = SQLAlchemy(metadata=metadata)
 class Bakery(db.Model, SerializerMixin):
     __tablename__ = 'bakeries'
 
+    serialize_rules = ('-bakedgoods.bakeries',)
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    bakedgoods = db.relationship('BakedGood', backref='bakeries')
 
 class BakedGood(db.Model, SerializerMixin):
     __tablename__ = 'baked_goods'
 
+    serialize_rules = ('-bakeries.bakedgoods',)
     id = db.Column(db.Integer, primary_key=True)
-    
+    name = db.Column(db.String)
+    price = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    bakery_id = db.Column(db.Integer, db.ForeignKey('bakeries.id'))
